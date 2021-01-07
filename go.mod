0a15655b695e8e11d7d302af1b8e6e3c3b97023b
lucasew <lucas59356@gmail.com>
2021-01-07 14:08:29 -0300

    genesis
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lucasew/redial_proxy
+
+go 1.15
+
+require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+)
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+
+	"log"
+
+	"github.com/armon/go-socks5"
+)
+
+var PORT int
+
+func init() {
+    flag.IntVar(&PORT, "p", 8889, "port to listen the server")
+    flag.Parse()
+}
+
+func main() {
+    log.Printf("starting...")
+    sconfig := socks5.Config{
+        Dial: func(ctx context.Context, network, addr string) (net.Conn, error) {
+            try := 0
+            for {
+                select {
+                case <-ctx.Done():
+                    return nil, fmt.Errorf("timeout")
+                default:
+                    if try > 3 {
+                        return nil, fmt.Errorf("too many retries")
+                    }
+                    conn, err := net.Dial(network, addr)
+                    if err != nil {
+                        log.Printf("conn err '%s'", err.Error())
+                        if strings.Contains(err.Error(), "route") {
+                            log.Printf("retrying connection to %s %s (%d)", network, addr, try)
+                            time.Sleep(100*time.Millisecond)
+                            try++
+                            continue
+                        }
+                        return nil, err
+                    }
+                    log.Printf("CONNECT %s %s", network, addr)
+                    return conn, err
+                }
+            }
+        },
+    }
+    srv, err := socks5.New(&sconfig)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("binding port %d...", PORT)
+    err = srv.ListenAndServe("tcp", fmt.Sprintf("127.0.0.1:%d", PORT))
+    if err != nil {
+        log.Fatal(err)
+    }
+}
