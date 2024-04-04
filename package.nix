53855fd1b1e42a3ea5a82c6251dfa3010084c418
lucasew <lucas59356@gmail.com>
2024-04-04 13:40:53 -0300

    nix: fix
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- package.nix
+++ package.nix
@@ -1,10 +1,13 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.buildGoModule rec {
+{ buildGoModule
+, lib
+}:
+
+buildGoModule {
   name = "redial_proxy";
   version = "0.0.1";
-  vendorSha256 = "sha256-0cdpO8+E4HD83SmMwoYOaMNhKCJXSwDmgnz/Uz/6Ka0=";
+  vendorHash = "sha256-0cdpO8+E4HD83SmMwoYOaMNhKCJXSwDmgnz/Uz/6Ka0=";
   src = ./.;
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "SOCKS5 compatible proxy that retries 3 times all requests that gives any error that have the word route. It is a workaround to a internet problem I am having.";
     homepage = "https://github.com/lucasew/redial_proxy";
     platforms = platforms.linux;
