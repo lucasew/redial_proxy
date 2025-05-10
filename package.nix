c457ee3d51fc7306e97c54158ddd192ed416d8e6
A helpful machine <actions@github.com>
2025-05-10 02:59:00 +0000

    Update package dependencies + tidy
    Automatic update to the project's package dependencies initiated by an
    automatic GitHub Action running on cron. Keeps upgrades less of a
    monolithic task and lets security-related patches trickle in more
    quickly.

--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@
 buildGoModule {
   name = "redial_proxy";
   version = "0.0.1";
-  vendorHash = "sha256-PR3zEwn010xcxyqGVYR1H904jMdapTRdatmeXfefbFs=";
+  vendorHash = "sha256-VZ4GsenuLUnSYrB8FBKHc5cKQizKa25X/nKTZ+PrPcE=";
   src = ./.;
   meta = with lib; {
     description = "SOCKS5 compatible proxy that retries 3 times all requests that gives any error that have the word route. It is a workaround to a internet problem I am having.";
