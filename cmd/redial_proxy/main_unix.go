a972880282a74221014547812f96916aa6804f98
lucasew <lucas59356@gmail.com>
2025-09-13 13:16:23 -0300

    allow socket activation in all unix platforms to see how it goes
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmd/redial_proxy/main_unix.go
+++ cmd/redial_proxy/main_unix.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"net"
+
+	"blitiri.com.ar/go/systemd"
 )
 
 func GetListener() (net.Listener, error) {
+	listeners, err := systemd.Listeners()
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range listeners {
+		for _, listener := range v {
+			return listener, nil
+		}
+	}
 	return net.Listen("tcp", fmt.Sprintf(":%d", PORT))
 }
