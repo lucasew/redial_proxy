eb559fdce0f59625d64d0d52d43974d666359832
A helpful machine <actions@github.com>
2025-09-13 02:40:59 +0000

    Update package dependencies + tidy
    Automatic update to the project's package dependencies initiated by an
    automatic GitHub Action running on cron. Keeps upgrades less of a
    monolithic task and lets security-related patches trickle in more
    quickly.

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/lucasew/redial_proxy
 
-go 1.23.0
+go 1.24.0
 
 toolchain go1.25.1
 
 require github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
 
-require golang.org/x/net v0.43.0 // indirect
+require golang.org/x/net v0.44.0 // indirect
--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@
 buildGoModule {
   name = "redial_proxy";
   version = "0.0.1";
-  vendorHash = "sha256-YG/K1giWdVtf2VlgQznW17JYX8NaZihTu0xhkeae/LI=";
+  vendorHash = "sha256-NxIOdmFGM97CkIGW5Kcr4eFuwT1s5+wOThUwOqttm8Y=";
   src = ./.;
   meta = with lib; {
     description = "SOCKS5 compatible proxy that retries 3 times all requests that gives any error that have the word route. It is a workaround to a internet problem I am having.";
